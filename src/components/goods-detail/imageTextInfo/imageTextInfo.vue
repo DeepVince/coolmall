3ef2d4ba7ad5ad85c9166a2970fad8f943b03756
DeepVince <1271317072@qq.com>
2018-07-21 17:50:27 +0800

    提交上一次提交失败的文件

---
+++ src/components/goods-detail/imageTextInfo/imageTextInfo.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="image-text-info">
+    我是图文详情组件
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
---
+++ src/components/goods-detail/specParameter/specParameter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="spec-parameter">
+    我是规格参数组件
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
